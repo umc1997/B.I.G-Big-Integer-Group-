2f5405515efc2a1c1d1dd9de6fa2f66bbc67016b
umc1997 <umc1997@kookmin.kr>
2020-11-11 15:08:06 +0900

    2020/11/11 gcd function done

--- ErrorMessage.c
+++ ErrorMessage.c
@@ -19,8 +19,10 @@ void printErrorMessage(ErrorMessage msg) {
 		break;
 	case(FAIL_INVALID_CHARACTER):
 		printf("Error: Please enter valid character (<bin: 0, 1> ,<dec:0 ~ 9>, <hex:0 ~ f>) (INVALID_CHARACTER) .\n");
+		break;
 	case(FAIL_INVALID_DIVISOR):
 		printf("Error: Divisor must be a positive number (INVALID_DIVISOR) .\n");
+		break;
 	case(FAIL_INVALID_EXPONENT):
 		printf("Error: Exponent must be a non-negative number (INVALID_EXPONENT) .\n");
 		break;
