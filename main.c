a4a96061f5acd5f8d4d7d5bbb7bd295ef154a705
umc1997 <umc1997@kookmin.kr>
2020-11-20 18:39:35 +0900

    2020/11/20 Update operation speed comparison

--- main.c
+++ main.c
@@ -13,12 +13,13 @@ int main()
 	bigint* a = NULL;
 	bigint* b = NULL;
 	bigint* c = NULL;
+	bigint* n = NULL;
 
 	int testCase = 1000;
 	int bit = 1024;
 	int aWordlen = bit / WORD_UNIT; // 1024 - bit
 	int bWordlen = bit / WORD_UNIT; // 1024 - bit
-
+	int nWordlen = bit / WORD_UNIT; // 1024 - bit
 
 #if testMode == 1
 	for (int i = 0; i < 5; i++) {
@@ -27,14 +28,30 @@ int main()
 		for (int t = 0; t < testCase; t++) {
 			/* generate random number */
 			big_gen_rand(&a, NON_NEGATIVE, aWordlen);
+			//do
+			//{
 			big_gen_rand(&b, NON_NEGATIVE, bWordlen);
+			//} while (big_is_zero(b));
+			//big_gen_rand(&n, NON_NEGATIVE, nWordlen);
 			
 			/* operation */
-			// do nothing
+			//big_mod_exp(&c, a, n, b);
 			
 #if testMode == 0
 			/* show */
-			showProcessHex(a, b, c);
+			//showProcessHex(a, b, c);
+			printf("a = ");
+			big_show_hex(a);
+			printf("b = ");
+			big_show_hex(b);
+			printf("n = ");
+			big_show_hex(n);
+			printf("c = ");
+			big_show_hex(c);
+			printf("if pow(a,n,b) == c :\n\t");
+			printf("print(\"True\")\n");
+			printf("else:\n");
+			printf("\tprint(\"False\")\n");
 #endif
 		}
 #if testMode == 1
@@ -48,6 +65,7 @@ int main()
 	big_delete(&a);
 	big_delete(&b);
 	big_delete(&c);
+	big_delete(&n);
 	return 0;
 }
 void showProcessHex(bigint* a, bigint* b, bigint* c)
